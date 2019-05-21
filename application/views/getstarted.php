1b3e067cd1bcb68afd2bf0c0a8aba59e59ad6fec
Nathan Malachowski <nathanmal@gmail.com>
2019-05-21 00:45:02 -0500

    added default view file

---
+++ application/views/getstarted.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Welcome to Codeigniter X</title>
+  <?= Asset::link('cix/dist/cix.css') ?>
+</head>
+<body class="ci-get-started">
+
+<main id="content">
+  <h1>Codeigniter X</h1>
+</main>
+
+
+</body>
+</html>
