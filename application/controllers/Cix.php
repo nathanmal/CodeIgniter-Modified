212d83b3d49c9c88bbbd88ee8add6c858ccf64c5
Nathan Malachowski <nathanmal@gmail.com>
2019-05-29 01:07:32 -0500

    command controller

--- application/controllers/Cix.php
+++ application/controllers/Cix.php
@@ -1,10 +1,11 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
+require_once( APPPATH . 'core/Command.php');
+
 class Cix extends Command 
 {
   public function index()
   {
-    echo 'CIX';
-    echo PHP_EOL;
+    
   }
 }
